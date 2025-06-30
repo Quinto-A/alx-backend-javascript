13033c95d1404d94a6f0449be7269659297492bd
Quinto-A <acquinoquinto5@gmail.com>
2025-06-30 22:54:15 +0300

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+/** program to be executed through command line
+ */
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+r1.on('line', (input) => {
+  process.stdout.write(`Your name is: ${input}\n`);
+  r1.close();
+});
+
+r1.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+process.on('exit', () => {
+});
