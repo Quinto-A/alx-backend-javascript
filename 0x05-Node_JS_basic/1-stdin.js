274ec32149a57f29a2e9daffc75ee81b400bdad3
Quinto-A <acquinoquinto5@gmail.com>
2025-06-30 23:13:12 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const readline = require('readline');
 
 const r1 = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 process.stdout.write('Welcome to ALX, what is your name?\n');
